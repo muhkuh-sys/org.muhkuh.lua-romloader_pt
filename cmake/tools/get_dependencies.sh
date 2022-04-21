a6a55357e8e4bc4039be56103aa0d793967253e3
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2022-04-21 16:48:44 +0200

    Add ARMHF builds.

--- cmake/tools/get_dependencies.sh
+++ cmake/tools/get_dependencies.sh
@@ -12,3 +12,10 @@ do
 		ln -fs ${PKG_DIR}${TARGET} ${LINK}
 	fi
 done
+
+# Remove any local installed versions of libc and libpthread. These libs are
+# shipped with the compiler. Local versions in the "packages" folder might
+# conflict with the compiler builtins.
+find . -name "libc*" -delete
+find . -name "libpthread*" -delete
+find . -name "stdio.h" -delete
